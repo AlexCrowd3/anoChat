202097d34092c64998fc83844fc3dc40cfa9450a
Александр <sasatkachev06@gmail.com>
2025-03-06 00:56:58 +0300

    Initial commit

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 var mode = 2;
 const urlParams = new URLSearchParams(window.location.search);
 let main_id;
+urlParam = parseInt(urlParams.get('main_id'))
 
 var global_user_count;
 let intervalId;
@@ -37,7 +38,7 @@ async function executeQuery(sql, params = []) {
     }
 }
 window.addEventListener('load', () => {
-    executeQuery('SELECT * FROM users WHERE user_id == ?', [parseInt(urlParams.get('main_id'))])
+    executeQuery('SELECT * FROM users WHERE telegram_id == ?', [urlParam])
         .then(rows => {
             if (rows.length != 0) {
                 main_id = rows.id;
